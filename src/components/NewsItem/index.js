1619f7e8e5de7d01bd4f2a703f0b2a0ba5aa2848
Chip Cerio <alistair.cerio@gmail.com>
2018-07-17 09:24:27 +0800

    removed redundant files

--- src/components/NewsItem/index.js
+++ src/components/NewsItem/index.js
@@ -1,3 +1,45 @@
-import NewsItem from './NewsItem';
+import React, { Component } from 'react';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+import moment from 'moment';
+import styles from './styles';
+
+class NewsItem extends Component {
+  constructor(props) {
+    super();
+    this.image = props.urlToImage == null ? 'http://via.placeholder.com/128x96' : props.urlToImage;
+    this.title = props.title;
+    this.description = props.description;
+    this.source = props.source;
+    this.date = moment(props.publishedAt).format('ddd, DD MMM');
+    this.navigation = props.navigation;
+  }
+
+  onPress = () =>
+    this.props.navigation.navigate('Details', {
+      title: this.title,
+      description: this.description,
+      source: this.source,
+      image: this.image,
+      date: this.date,
+    });
+
+  render() {
+    const { rootViewStyle, imageViewStyle, textViewStyle, titleStyle, dateStyle } = styles;
+
+    return (
+      <View style={rootViewStyle}>
+        <Image source={{ uri: this.image }} style={imageViewStyle} />
+        <View style={textViewStyle}>
+          <TouchableOpacity onPress={this.onPress}>
+            <Text style={titleStyle} numberOfLines={3}>
+              {this.title}
+            </Text>
+            <Text style={dateStyle}>{this.date}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default NewsItem;
--- src/components/SourceList/index.js
+++ src/components/SourceList/index.js
@@ -1,3 +1,39 @@
-import SourceList from './SourceList';
+import React, { Component } from 'react';
+import { FlatList, View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions';
+import SourceItem from '../SourceItem';
 
-export default SourceList;
+class SourceList extends Component {
+  componentDidMount() {
+    this.props.actions.getSources();
+  }
+
+  renderSourceItem = ({ item }) => <SourceItem title={item.name} />;
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.props.sources}
+          keyExtractor={item => item.id}
+          renderItem={this.renderSourceItem}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  sources: state.source.sources,
+  empty: state.source.empty,
+  onLoading: state.source.onLoading,
+  onError: state.source.onError,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SourceList);
