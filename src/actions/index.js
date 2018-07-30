b22b824e12df086622c730381f8e4d444141a541
Chip Cerio <alistair.cerio@gmail.com>
2018-07-31 01:06:56 +0800

    changed network request to async-await

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,28 +2,26 @@ import axios from 'axios';
 import { NEWS_API } from 'react-native-dotenv';
 import { NEWS_LIST, ON_LOADING, ON_ERROR, SOURCE_LIST } from './types';
 
-export const getArticles = () => dispatch => {
-  dispatch({ type: ON_LOADING });
-
-  axios
-    .get(`https://newsapi.org/v2/everything?sources=bloomberg&page=1&apiKey=${NEWS_API}`)
-    .then(response => {
-      dispatch({ type: NEWS_LIST, payload: response.data.articles });
-    })
-    .catch(() => {
-      dispatch({ type: ON_ERROR, payload: 'Error in loading articles' });
-    });
+export const getArticles = () => async dispatch => {
+  try {
+    dispatch({ type: ON_LOADING });
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?sources=bloomberg&page=1&apiKey=${NEWS_API}`,
+    );
+    dispatch({ type: NEWS_LIST, payload: response.data.articles });
+  } catch (error) {
+    dispatch({ type: ON_ERROR, payload: 'Error in loading articles' });
+    throw new Error(error);
+  }
 };
 
-export const getSources = () => dispatch => {
-  dispatch({ type: ON_LOADING });
-
-  axios
-    .get(`https://newsapi.org/v2/sources?apiKey=${NEWS_API}`)
-    .then(response => {
-      dispatch({ type: SOURCE_LIST, payload: response.data.sources });
-    })
-    .catch(() => {
-      dispatch({ type: ON_ERROR, payload: 'Error in loading sources' });
-    });
+export const getSources = () => async dispatch => {
+  try {
+    dispatch({ type: ON_LOADING });
+    const response = await axios.get(`https://newsapi.org/v2/sources?apiKey=${NEWS_API}`);
+    dispatch({ type: SOURCE_LIST, payload: response.data.sources });
+  } catch (error) {
+    dispatch({ type: ON_ERROR, payload: 'Error in loading sources' });
+    throw new Error(error);
+  }
 };
